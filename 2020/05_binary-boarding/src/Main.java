f8d622ccd31b88ba295733b96d63cf349c890123
Xev <xevnar@gmail.com>
2020-12-19 15:48:10 +0300

    Day 05: Completed part 1

--- 2020/05_binary-boarding/src/Main.java
+++ 2020/05_binary-boarding/src/Main.java
@@ -10,6 +10,24 @@ public class Main {
 
     public static void main(String [] args) {
         parseFile();
+        partA();
+    }
+
+    static void partA() {
+        int largestSeatID = Integer.MIN_VALUE;
+        for (int i = 0; i < seats.length; i++) {
+            /*
+             * replaceChars(seats [i], "FLBR", "0011") from the apache commons
+             * library is a nicer alterantive
+             */
+            String binary = seats [i].replace('F', '0').replace('B', '1')
+                                     .replace('L', '0').replace('R', '1');
+
+            int seatID = Integer.parseInt(binary, 2);
+            largestSeatID = (seatID > largestSeatID) ? seatID : largestSeatID;
+        }
+
+        System.out.printf("The highest seat ID is %d%n", largestSeatID);
     }
 
     static void parseFile() {
