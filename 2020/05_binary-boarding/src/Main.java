c381e6e82712d700755448a2b9a663a51d3c224d
Xev <xevnar@gmail.com>
2020-12-19 16:06:14 +0300

    Day 05: Completed part 2

--- 2020/05_binary-boarding/src/Main.java
+++ 2020/05_binary-boarding/src/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     private static final File file = new File("input.txt");
@@ -11,23 +12,40 @@ public class Main {
     public static void main(String [] args) {
         parseFile();
         partA();
+        partB();
     }
 
     static void partA() {
-        int largestSeatID = Integer.MIN_VALUE;
+        int [] seatIDs = getSeatIDs();
+        Arrays.sort(seatIDs);
+        System.out.printf("The highest seat ID is %d%n", seatIDs [seatIDs.length - 1]);
+    }
+
+    static void partB() {
+        int [] seatIDs = getSeatIDs();
+        Arrays.sort(seatIDs);
+
+        for (int i = 0; i < seatIDs.length - 1; i++) {
+            if ((seatIDs [i] + 1) != seatIDs [i + 1]
+                    && (seatIDs [i] + 2) == seatIDs [i + 1]) {
+                System.out.printf("Your seat ID is %d%n", seatIDs [i] + 1);
+            }
+        }
+    }
+
+    static int [] getSeatIDs() {
+        int [] seatIDs = new int [seats.length];
         for (int i = 0; i < seats.length; i++) {
             /*
              * replaceChars(seats [i], "FLBR", "0011") from the apache commons
-             * library is a nicer alterantive
+             * library is a nicer alternative
              */
             String binary = seats [i].replace('F', '0').replace('B', '1')
                                      .replace('L', '0').replace('R', '1');
 
-            int seatID = Integer.parseInt(binary, 2);
-            largestSeatID = (seatID > largestSeatID) ? seatID : largestSeatID;
+            seatIDs [i] = Integer.parseInt(binary, 2);
         }
-
-        System.out.printf("The highest seat ID is %d%n", largestSeatID);
+        return seatIDs;
     }
 
     static void parseFile() {
