2f3cf7a1f491893216780ccbbbe9da6140c2beff
Xev <xevnar@gmail.com>
2020-12-19 15:13:06 +0300

    Day 04: Completed part 2

--- 2020/04_passport-processing/src/Main.java
+++ 2020/04_passport-processing/src/Main.java
@@ -11,10 +11,71 @@ public class Main {
     public static void main(String [] args) {
         parseFile();
         partA();
+        partB();
     }
 
     static void partA() {
+        String [] completePassports = getCompletePassports();
+
+        System.out.printf("The number of complete passports is %d%n",
+                completePassports.length);
+    }
+
+    static void partB() {
+        String [] completePassports = getCompletePassports();
+
         ArrayList<String> validPassports = new ArrayList<>();
+        passport_validating:
+        for (int i = 0; i < completePassports.length; i++) {
+            String [] currentPassport = completePassports [i].split("(\\v|\\h)+");
+
+            for (int j = 0; j < currentPassport.length; j++) {
+                String [] info = currentPassport [j].split(":");
+                String regex;
+                switch (info [0]) {
+                    case "byr":
+                        regex = "(19[2-9]\\d|200[0-2])";
+                        break;
+                    case "iyr":
+                        regex = "20(1\\d|20)";
+                        break;
+                    case "eyr":
+                        regex = "20(2\\d|30)";
+                        break;
+                    case "hgt":
+                        regex = "(1([5-8]\\d|9[0-3])cm|(59|6\\d|7[0-6])in)";
+                        break;
+                    case "hcl":
+                        regex = "#[0-9a-f]{6}";
+                        break;
+                    case "ecl":
+                        regex = "(amb|blu|brn|gry|grn|hzl|oth)";
+                        break;
+                    case "pid":
+                        regex = "\\d{9}";
+                        break;
+                    case "cid":
+                        regex =".*";
+                        break;
+                    default:
+                        regex = "\\w\\b\\w"; // unmatchable regex
+                        break;
+                }
+
+                if (! info [1].matches(regex)) {
+                    // Invalid Passport
+                    continue passport_validating;
+                }
+            }
+
+            validPassports.add(completePassports [i]);
+        }
+        System.out.printf("The amount of valid passports is %d%n",
+                validPassports.size());
+    }
+
+    static String [] getCompletePassports() {
+        ArrayList<String> completePassports = new ArrayList<>();
 
         for (int i = 0; i < passports.length; i++) {
             String [] currentPassport = passports [i].split("(\\v|\\h)+");
@@ -37,11 +98,9 @@ public class Main {
                 }
             }
 
-            validPassports.add(passports [i]);
+            completePassports.add(passports [i]);
         }
-
-        System.out.printf("The number of valid passports is %d%n",
-                validPassports.size());
+        return completePassports.toArray(new String [completePassports.size()]);
     }
 
     static void parseFile() {
