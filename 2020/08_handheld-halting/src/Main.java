01afdf808fc90d1a1f5933dfe489ff942268ac84
Xev <xevnar@gmail.com>
2021-01-10 18:50:05 +0300

    Day 08: Completed part 2

--- 2020/08_handheld-halting/src/Main.java
+++ 2020/08_handheld-halting/src/Main.java
@@ -17,6 +17,11 @@ public class Main {
     }
 
     static void partA() {
+        System.out.printf("Accumulator value before looping is %d%n",
+                boot(bootCode.clone()));
+    }
+
+    static int boot(String [] bootCode) {
         int accumulator = 0;
         for (int i = 0; i < bootCode.length;) {
             if (bootCode [i].equals("")) {
@@ -40,15 +45,69 @@ public class Main {
                     break;
             }
         }
-        System.out.printf("Accumulator value before loop is %d%n", accumulator);
+        return accumulator;
     }
 
     static void partB() {
+        repairBootCode();
+        System.out.printf("Accumulator value after fix is %d%n",
+                boot(bootCode.clone()));
+    }
+
+    static void repairBootCode() {
+        String [] localBootCode = bootCode.clone();
+
+        for (int i = 0; i < localBootCode.length;) {
+            boolean testOutcome = false;
+            if (!localBootCode [i].startsWith("acc")) {
+                String [] temp = localBootCode.clone();
+
+                String fix = (temp [i].startsWith("nop") ? "jmp" : "nop")
+                    + temp [i].substring(3);
+                temp [i] = fix;
+
+                if (testFix(temp, i)) {
+                    bootCode [i] = fix;
+                    return;
+                }
+            }
+
+            switch (localBootCode [i].substring(0, 3)) {
+                case "jmp":
+                    int newIndex = Integer.parseInt(localBootCode [i].substring(4));
+                    localBootCode [i] = "";
+                    i += newIndex;
+                    break;
+                default:
+                    localBootCode [i++] = "";
+                    break;
+            }
+        }
+    }
+
+    static boolean testFix(String [] localBootCode, int startIndex) {
+        for (int i = startIndex; i < localBootCode.length;) {
+            if (localBootCode [i].equals("")) {
+                return false;
+            }
+
+            switch (localBootCode [i].substring(0, 3)) {
+                case "jmp":
+                    int newIndex = Integer.parseInt(localBootCode [i].substring(4));
+                    localBootCode [i] = "";
+                    i += newIndex;
+                    break;
+                default:
+                    localBootCode [i++] = "";
+                    break;
+            }
+        }
+        return true;
     }
 
     static void parseFile() {
         try (Scanner input = new Scanner(file)) {
-            List <String> bootCode = new LinkedList<>();
+            List<String> bootCode = new LinkedList<>();
             input.useDelimiter("\n");
             while (input.hasNext()) {
                 bootCode.add(input.next());
